a405153ec2a4bfd4984d042e31fae71c264d3403
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-09 14:48:18 +0630

    added: swagger and inceptors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { HttpResponseException } from 'src/utils/http-exception.filter';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,10 +9,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: false,
   });
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  app.useGlobalFilters(new HttpResponseException());
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Frequency Streaming')
+    .setDescription('API description for Frequency Streaming Service')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(PORT, () => {
     console.log(`Nest server is running on port ${PORT}`);
   });
