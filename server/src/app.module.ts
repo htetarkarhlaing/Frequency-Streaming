223497381d922b4e6b6834f01d206d5da1f8edb7
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-09 15:00:14 +0630

    added: health service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PeerModule } from './peer/peer.module';
 import { EventsModule } from './events/events.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PeerModule, EventsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PeerModule,
+    EventsModule,
+    HealthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
