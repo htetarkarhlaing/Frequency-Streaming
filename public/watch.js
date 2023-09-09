6fb265b020ba8089204b23dea2fea2fe7ad81e2a
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-09 11:32:33 +0630

    deleted: dist and changed env

--- public/watch.js
+++ public/watch.js
@@ -4,11 +4,6 @@ const config = {
       { 
         "urls": "stun:stun.l.google.com:19302",
       },
-      // { 
-      //   "urls": "turn:TURN_IP?transport=tcp",
-      //   "username": "TURN_USERNAME",
-      //   "credential": "TURN_CREDENTIALS"
-      // }
   ]
 };
 
