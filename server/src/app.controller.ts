3438aa002a964e6e3c5cf550e2bc2696e2844e71
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-11 15:43:58 +0630

    updated: Swagger API tag

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { Response } from 'express';
 import { AppService } from './app.service';
 
@@ -7,16 +8,19 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiTags('Public')
   getHello(): string {
     return this.appService.getHello();
   }
 
   @Get('/public/:fileName')
+  @ApiTags('Public')
   publicFileResolver(@Param('fileName') name: string, @Res() res: Response) {
     return res.sendFile(name, { root: `./public/` });
   }
 
   @Get('/uploads/:fileName')
+  @ApiTags('Public')
   uploadFileResolver(@Param('fileName') name: string, @Res() res: Response) {
     return res.sendFile(name, { root: `./uploads/` });
   }
