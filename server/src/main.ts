c58de64392eeb37d1b41376024a332d7a48be21d
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-12 13:33:13 +0630

    updated: HBS setup

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { HttpResponseException } from 'src/utils/http-exception.filter';
+import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 8000;
-  const app = await NestFactory.create(AppModule, {
-    logger: false,
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    // logger: false,
   });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
@@ -17,8 +19,11 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpResponseException());
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api', {
-    exclude: ['/', '/public/:fileName', '/uploads/:fileName', 'health'],
+    exclude: ['/', '/uploads/:fileName', 'health'],
   });
+  app.useStaticAssets(join(__dirname, '../..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '../..', 'views'));
+  app.setViewEngine('hbs');
   const config = new DocumentBuilder()
     .setTitle('Frequency Streaming')
     .setDescription('API description for Frequency Streaming Service')
