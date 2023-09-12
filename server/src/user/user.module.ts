99fb8a319e815233de49dbe19c2c9a200eee35c3
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-12 15:03:03 +0630

    added: user register service with mail support

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, PrismaService],
+  providers: [UserService, PrismaService, JwtService],
 })
 export class UserModule {}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,20 +1,64 @@
 import { Injectable, HttpException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 import { UserCreate } from './dto';
 import { Responser } from 'src/utils/Responser';
+import { ConfigService } from '@nestjs/config';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   async userInviteService(data: UserCreate) {
     try {
-      return Responser({
-        statusCode: 201,
-        message: 'User invited successfully.',
-        devMessage: 'new-user-invited',
-        body: null,
+      const isUserExisting = await this.prismaService.user.findFirst({
+        where: {
+          email: data.email,
+        },
       });
+      if (isUserExisting) {
+        throw new HttpException(
+          {
+            message: 'User with the provided email already exists.',
+            devMessage: 'email-already-used',
+          },
+          409,
+        );
+      } else {
+        const token = await this.jwtService.signAsync(
+          {
+            name: data.name,
+            email: data.email,
+          },
+          {
+            secret: this.configService.get('JWT_REGISTER_SECRET'),
+            expiresIn: '1d',
+          },
+        );
+        await this.mailerService.sendMail({
+          to: data.email,
+          subject: 'Testing Nest Mailermodule with template ✔',
+          template: 'register',
+          context: {
+            subject: 'User register',
+            appURL: this.configService.get('APP_URL'),
+            token: token,
+          },
+        });
+
+        return Responser({
+          statusCode: 201,
+          message: 'User invited successfully.',
+          devMessage: 'new-user-invited',
+          body: null,
+        });
+      }
     } catch (err) {
       throw new HttpException(
         { message: 'Internal server error occurred', devMessage: err.message },
