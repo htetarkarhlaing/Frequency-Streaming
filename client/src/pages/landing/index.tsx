37d7a1acd1bce30163329b684d35372307a976da
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-13 16:30:08 +0630

    added: landing page

--- client/src/pages/landing/index.tsx
+++ client/src/pages/landing/index.tsx
@@ -1,6 +1,19 @@
+import { useDisclosure } from "@nextui-org/react";
+import Login from "../../components/login"
+import Nav from "../../components/nav"
+import Register from "../../components/register";
+
 const Landing = () => {
+
+    const loginDisclosure = useDisclosure();
+    const registerDisclosure = useDisclosure();
+
     return (
-        <div>Landing</div>
+        <div className="w-screen h-screen">
+            <Nav openLogin={loginDisclosure.onOpen} openRegister={registerDisclosure.onOpen} />
+            <Login isOpen={loginDisclosure.isOpen} onOpenChange={loginDisclosure.onOpenChange} />
+            <Register isOpen={registerDisclosure.isOpen} onOpenChange={registerDisclosure.onOpenChange} />
+        </div>
     )
 }
 
