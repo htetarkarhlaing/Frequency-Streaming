1372cb1a246b198c173123d78f4cf8b8c38e4f07
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-15 11:44:11 +0630

    fixed: auth system - route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -105,6 +105,9 @@ const Routes = () => {
                 refreshToken: cookieRefreshToken
             })
         }
+        else {
+            dispatch(ReduxLogout())
+        }
 
     }, [cookieAccessToken, cookieRefreshToken])
 
