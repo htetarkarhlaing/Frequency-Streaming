5f99ab509c89e68026cc9c18b6a3a28fd0035d0c
htetarkarhlaing <htet.arkar.hlaing.official.mm@gmail.com>
2023-09-16 10:34:49 +0630

    bugfix: fixed import error

--- server/src/channel/channel.module.ts
+++ server/src/channel/channel.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ChannelService } from './channel.service';
 import { ChannelController } from './channel.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [ChannelController],
-  providers: [ChannelService, PrismaService],
+  providers: [ChannelService, PrismaService, JwtService],
 })
 export class ChannelModule {}
